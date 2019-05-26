23be98dc02698a9fae6cbb0cfc693e37fba45345
Tim Isenman <tim.isenman@gmail.com>
2019-05-26 22:23:15 +0900

    Add instructional comments for creating gradient views

--- GradientPlay/ViewController.swift
+++ GradientPlay/ViewController.swift
@@ -15,16 +15,27 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         self.navigationController?.isNavigationBarHidden = true
         self.view.backgroundColor = .white
+        
+        //Add Views to superView
+        //Adding subviews must occur before constraints are added
         self.view.addSubview(topView)
         self.view.addSubview(secondView)
         self.view.addSubview(thirdView)
         self.view.addSubview(fourthView)
         self.view.addSubview(fifthView)
         
+        //Declare frame-based values to use as view dimensions
         let width = self.view.frame.width
         let yMid = self.view.frame.minY
+        
+        //Set arbitrary height - Adjust at your descretion
         let height: CGFloat = 163
         
+        //Add each individual view
+        //Similar to above - Must define the constraints after these views
+            //have been added as subviews
+        //Add the height of each view multiplied by the position it's in to make
+            //a perfectly flush array of gradients
         topView.frame = CGRect(x: 0, y: yMid, width: width, height: height)
         topView.setGradient(colorOne: UIColor.red, colorTwo: UIColor.orange)
         
@@ -41,6 +52,7 @@ class ViewController: UIViewController {
         fifthView.setGradient(colorOne: UIColor.blue, colorTwo: UIColor.cyan)
     }
     
+    //Declare individual views AFTER viewDidLoad. They won't appear otherwise.
     let topView: UIView = {
         let newView = UIView()
         return newView
@@ -65,12 +77,11 @@ class ViewController: UIViewController {
         let newView = UIView()
         return newView
     }()
-    
-    
-
 
 }
 
+//Add this extension to UIView class to allow any
+    //subclass of UIView to inherit the same behavior
 extension UIView {
     func setGradient(colorOne: UIColor, colorTwo: UIColor) {
         let gradientLayer = CAGradientLayer()
